722b127f053cad5105c5ad3258800f49c62ac0c7
The-Unknown-077 <gamergamervich@gmail.com>
2025-08-19 16:24:04 +0500

    Added local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import savedReducer from "../features/bookmark/redux/saved";
 
+const loadFromLocalStorage = () => {
+  const data = localStorage.getItem("savedMovies");
+  if (data) {
+    return { saved: JSON.parse(data) };
+  }
+  return undefined;
+};
+
+const saveToLocalStorage = (state: any) => {
+  localStorage.setItem("savedMovies", JSON.stringify(state.saved));
+};
+
 export const store = configureStore({
   reducer: {
     saved: savedReducer,
   },
+  preloadedState: loadFromLocalStorage(),
+});
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
 });
 
 export type RootState = ReturnType<typeof store.getState>;
